7b1fea21e8c2ac012839c26e86c5308193aabd04
Neriek <neriekp@gmail.com>
2015-01-28 19:36:34 +1000

    larger list of references

---
+++ Reference Materials/kh2.5 codelist.cs
@@ -0,0 +1,332 @@
+Universal Character Modifier
+ 
+Character
+ 
+
+Sora Replaced By
+0 027F6A88 ????
+
+Donald Replaced By
+0 027F6A8A ????
+
+Goofy Replaced By
+0 027F6A8C ????
+
+Guest Replaced By
+0 027F6A8E ????
+
+Valor Form Replaced By
+0 027F6A90 ????
+
+Wisdom Form Replaced By
+0 027F6A92 ????
+
+Limit Form Replaced By
+0 027F6A94 ????
+
+Master Form Replaced By
+0 027F6A96 ????
+
+Final Form Replaced By
+0 027F6A98 ????
+
+Anti-Form Replaced By
+0 027F6A9A ????
+
+King Mickey Replaced By
+0 027F6A9C ????
+
+Roxas (Boss) Replaced By
+0 027C4866 ????
+
+Terra (Boss) Replaced By
+0 027CADF6 ????
+
+???? Digit
+ 
+
+Digit
+0054 - Sora
+0055 - Sora (hero form/Valor form)
+0056 - Sora (way form/Wisdom form)
+0057 - Sora (master form/master form)
+0058 - Sora (Über-Form/final form)
+0059 - Sora (anti-form)
+095D - Sora (Limit form)
+005A - Roxas
+005B - Micky (with Kutte/with robe)
+005C - Donald
+005D - Goofy
+005E - Beast
+005F - Jack (Halloween Town)
+0060 - Jack (Christmas town)
+0061 - Simba
+0062 - Aladdin
+0063 - Mulan
+0064 - Ping
+0065 - Auron
+0066 - Sparrow
+0067 - King trailer/Kingdom key (dummy)
+0068 - Star loyalty/Oath Keeper (dummy)
+0069 - Note Irish/Oblivion (dummy)
+006A - Glitchy KHI Ultima (dummy)
+006B - Glitchy KHI Ultima (dummy)
+006C - Magic wand (dummy)
+006D - Knight sign (dummy)
+006E - Sign (dummy)
+006F - Bent sword (dummy)
+0070 - Battle companion (dummy)
+0071 - Ancestor sword (dummy)
+0072 - Mushu (dummy)
+0073 - Dead head sword (dummy)
+0074 - Mickys key sword (dummy)
+0075 - Nothing?
+0076 - Gigg Schack (opponent! /Enemy!)
+0077 - Gigg Schack (opponent! /Enemy!)
+0078 - Schattenschalk (opponent! /Enemy!)
+0079 - Tetrabot (opponent! /Enemy!)
+007A - Morning star (opponent! /Enemy!)
+007B - Tornado dancer (opponent! /Enemy!)
+007C - Crescendo (opponent! /Enemy!)
+007D - Schauderplanze (opponent! /Enemy!)
+007E - Oogie Boogie (Boss!)
+007F - Fear (Boss!)
+0080 - Fear (Boss!)
+0081 - Fright (Boss!)
+0082 - Carpet/Carpet
+0083 - “Freeze”
+0084 - “Freeze”
+00C8 - Hero form/Valor form (dummy)
+0108 - Sora (dummy)
+02D4 - Tron
+0318 - Micky (without Kutte/robe without)
+0319 - Donald
+0323 - Roxas (hero form/Valor form)
+05F9 - Way form/Wisdom form (dummy)
+05FA - Master form/master form (dummy)
+05FB - Über-Form/final form (dummy)
+06C1 - Sora (Kingdom Hearts I)
+081D - Sparrow
+0819 - Riku (only guest/only Guest)
+095D - Limit form
+097F - Limit form (dummy)
+061C - Leon (Partner!)
+06B0 - Yuffie (Partner!)
+06B3 - Tifa (Partner!)
+0688 - Cloud (Partner!)
+016A - Hercules (Partner!)
+04BB - Minnie (Partner!)
+06C2 - Eeyore (Partner!)
+06C3 - Tigger (Partner!)
+06C4 - Piglet (Partner!)
+06C5 - Roo (Partner!)
+0764 - Mickey (without Coat) (Partner!)
+04DC - Axel (Partner!)
+0133 - Twilight Thorn (Boss!)
+04D7 - Hayner (Boss!)
+0678 - Seifer (Boss!)
+08B5 - Axel (Twilight Town, 1. Battle) (Boss!)
+082F - Setzer (Boss!)
+0754 - Roxas' Shadow Doppelgaenger (Boss!)
+0051 - Axel (Twilight Town, 2nd Battle ) (Boss!)
+0015 - Shan-Yu (Boss!)
+0016 - Hayabusa (Shan-Yu's Falcon) (Boss!)
+0161 - Thresholder (Boss!)
+02CE - Beast (Boss!)
+0162 - Dark Thorn (Boss!)
+0163 - Shadow Stalker (Boss!)
+0284 - Shadow Stalker Chandelier (Boss!)
+0285 - Shadow Stalker Pillars (Boss!)
+015F - Cerberus (Boss!)
+031B - Demyx (Olympus Coliseum, 1. Battle) (Boss!)
+08F6 - Demyx's Water Clone (Boss!)
+0160 - Hydra (Boss!)
+039A - Hydra's Head (Boss!)
+0167 - Pete (Timeless River) (Boss!)
+0166 - Illuminator (Boss!)
+03F7 - Barbossa (Boss!)
+040B - Volcano Lord (Boss!)
+040C - Blizzard Lord (Boss!)
+05CE - Prison Keeper (Boss!)
+007E - Oogie Boogie (Boss!)
+0299 - Shenzi (Boss!)
+029A - Banzai (Boss!)
+029B - Ed (Boss!)
+029C - Scar (Boss!)
+04B8 - The Hostile Program (Boss!)
+08F7 - Demyx (Hollow Bastion, 2. Battle) (Boss!)
+063C - Riku (Boss!)
+0165 - Storm Rider (Boss!)
+03E5 - Xaldin (Boss!)
+0812 - Hades (Boss!)
+0639 - Jafar's Water Clone (invincible) (Boss!)
+015C - Jafar (Boss!)
+007F - Lock (Boss!)
+0080 - Shock (Boss!)
+0081 - Barrel (Boss!)
+05D0 - The Experiment (Boss!)
+0459 - Groundshaker (Boss!)
+035E - Sark (Boss!)
+03C4 - Sark (Large) (Boss!)
+08F8 - Squall / Leon (Boss!)
+08F9 - Cloud (Boss!)
+08FA - Tifa (Boss!)
+08FB - Yuffie (Boss!)
+08B6 - Sephiroth (Boss!)
+015D - Hades (1st and 2nd Battle) (Boss!)
+015E - Hades (3rd Paradox Hades Cup Battle) (Boss!)
+0622 - Xigbar (Boss!)
+05F8 - Luxord (No HP-Bar) (Boss!)
+05FF - "Luxord's Card" (Boss!)
+
+Complete List= http://www.ultimate-cheatz.de/codeba...Digit_List.txt
+
+
+
+
+Model Modifier
+ 
+
+Sora
+0 0280E984 313030
+0 0280E987 xxxxxxxxxx
+
+
+Valor
+0 0280E9E4 313030
+0 0280E9E7 xxxxxxxxxx
+
+
+Wisdom
+0 0280EA44 313030
+0 0280EA47 xxxxxxxxxx
+
+
+Master
+0 0280EAA4 313030
+0 0280EAA7 xxxxxxxxxx
+
+
+Final
+0 0280EB04 313030
+0 0280EB08 xxxxxxxxxx
+
+
+xxxxxxxxxx
+
+Sora 0000000000
+Valor 5F42544C46
+Wisdom 5F4D414746
+Limit 5F4B483146
+Master 5F54524946
+Final 5F554C5446
+Anti 5F48544C46
+
+
+
+
+Roxas Ally
+0 0280ecdc 0100000E
+0 0280ece4 313130
+0 0280ed04 313130
+0 0280ed24 00030009
+
+
+
+
+Party Mod
+ 
+Worlds
+ 
+
+0 0077550C NNDDWWMM Twilight Town
+0 00775510 NNDDWWMM
+0 00775514 NNDDWWMM Hollow Bastion
+0 00775518 NNDDWWMM Beasts Castle
+0 0077551C NNDDWWMM Olympus Coliseum
+0 00775520 NNDDWWMM Agrabah
+0 00775524 NNDDWWMM The Land of Dragons
+0 00775528 NNDDWWMM 100 Acre Wood
+0 0077552C NNDDWWMM Pride Lands
+0 00775530 NNDDWWMM Atlantica
+0 00775534 NNDDWWMM Disney Castle
+0 00775538 NNDDWWMM Timeless River
+0 0077553c NNDDWWMM Halloween Town
+0 00775540 NNDDWWMM
+0 00775544 NNDDWWMM Port Royal
+0 00775548 NNDDWWMM Space Paranoids
+0 0077554C NNDDWWMM The World that Never Was
+
+
+Slots
+
+NN = Slot 1 (Main Slot - Sora/Roxas)
+DD = Slot 2
+WW = Slot 3
+MM = Slot 4
+
+00- Sora/Roxas (Roxas in his part).
+01- Donald
+02- Goofy
+03- World Character
+04- Sora Valor/Roxas Two Keyblades (Roxas in his part).
+05- Wisdom
+06- Limit Form
+07- Master
+08- Final
+09- Antiform
+0A- Mickey
+12- None
+
+
+
+
+
+Weapon Modifier
+ 
+Sora Weapon Mod
+0 007744C0 ????
+
+Valor Form Left Hand Weapon Mod
+0 007752C4 ????
+
+Master Form Left Hand Weapon Mod
+0 0077536C ????
+
+Final Form Left Hand Weapon Mod
+0 007753A4 ????
+
+Donald Weapon Mod
+0 007745D4 ????
+
+Goofy Weapon Mod
+0 007744C0 ????
+
+Mickey Weapon Mod
+0 007747FC ????
+
+Riku Weapon Mod
+0 007751B0 ????
+
+
+
+
+Reaction Command Modifier
+0 027F9BAA 00000000
+0 027F9BAE ????
+0 027F9BB1 00010001
+
+
+
+
+Shortcut Modifier
+0 007756C8 0aaa0bbb
+0 007756CC 0ccc0ddd
+
+
+
+
+Magic Slot Modifier
+0 027f9bd4 0aaa0bbb
+0 027f9bd8 0ccc0ddd
+0 027f9bdc 0eee0fff
